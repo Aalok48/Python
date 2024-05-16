0e6c4f488b79fe20a58ac558a1a7282b61cee463
Alok <karnaalok48@gmail.com>
2024-05-16 15:51:53 +0545

    super method in oop

---
+++ superoop.py
@@ -0,0 +1,25 @@
+# Parent class
+class Car:        
+    def __init__(self, type):
+        self.type = type   
+
+    @staticmethod
+    def start():
+        print("Car started...")
+
+    @staticmethod
+    def accelerate():
+        print("Car accelerating")
+
+    @staticmethod
+    def stop():
+        print("Car stopped...")
+
+class ToyotaCar(Car):
+    def __init__(self, name, type):
+        self.name = name
+        super().__init__(type)
+
+car1 = ToyotaCar("prius", "electric")
+print(car1.name)
+print(car1.type)
