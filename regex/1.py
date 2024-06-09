aa519882ff11b10cdeef27842b31e3d975e5d856
Alok <karnaalok48@gmail.com>
2024-06-09 18:40:58 +0545

    finditer and findall diff

---
+++ regex/1.py
@@ -0,0 +1,9 @@
+import re
+
+pattern =  'dog|cat'
+data = 'Dog and cat are two pet animals that I have but i prefer dog over cat.'
+match_data = re.finditer(pattern, data)
+for i in match_data:
+    print(i)
+
+print(re.findall(pattern, data))
